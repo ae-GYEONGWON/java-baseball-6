25829ecbd7c20e00660ad60a218b31387d75d457
ae-GYEONGWON <tjsxor50@gmail.com>
2023-11-26 15:30:27 +0900

    feat: 입력 받은 문자열이 숫자인지 확인

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -18,10 +18,28 @@ public class Player {
         return playerNumbersString;
     }
 
-    public List<Integer> getPlayerNumbers() {
+    private void convertStringToIntList() {
         for (char ch : playerNumbersString.toCharArray()) {
             playerNumbers.add(Character.getNumericValue(ch));
         }
+    }
+
+    public List<Integer> getPlayerNumbers() {
         return playerNumbers;
     }
+
+    private boolean validatePlayerNumbers() {
+        return is_Number();
+    }
+
+    private boolean is_Number() {
+        try{
+            Integer.parseInt(playerNumbersString);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
 }
